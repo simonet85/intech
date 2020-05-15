20c5f4b16b7e46fe421cde66cb2f590aeca1f1b8
Anthony Seure <anthony.seure@gmail.com>
2020-05-15 15:37:12 +0200

    tp011: add subject for ex3

---
+++ src/main/java/fr/aseure/tp011/ex3/App.java
@@ -0,0 +1,9 @@
+package fr.aseure.tp011.ex3;
+
+public class App {
+    public static void main(String[] args) throws InterruptedException {
+        Thread.sleep(10_000);
+        byte[] bytes = new byte[50_000_000];
+        Thread.sleep(3_600_000);
+    }
+}
